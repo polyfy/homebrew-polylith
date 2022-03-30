af025b7ecf14209ebd9cf94f773c2313b0ed9f70
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-03-30 18:18:58 +0200

    Release v0.2.14-alpha

--- Formula/poly@0.2.14-alpha.rb
+++ Formula/poly@0.2.14-alpha.rb
@@ -1,9 +1,11 @@
-class Poly < Formula
+class PolyAT0214Alpha < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.2.14-alpha/poly-0.2.14-alpha.tar.gz"
   sha256 "027cddf1f818027c1ec28d233167a689fec7c0ba49daaf84198841642fb6c9bf"
   license "EPL-1.0"
+
+  bottle :unneeded
 
   uses_from_macos "ruby" => :build
 
