84ca3ee1bcbeb04192f5eda816ad1cade0b889ec
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-12-08 22:47:05 +0100

    Release 0.2.16-alpha

--- Formula/poly@0.2.16-alpha.rb
+++ Formula/poly@0.2.16-alpha.rb
@@ -1,4 +1,4 @@
-class Poly < Formula
+class PolyAT0216Alpha < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.2.16-alpha/poly-0.2.16-alpha.tar.gz"
