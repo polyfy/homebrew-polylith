e641a1be4c4a494383c6970a42cc73f1f01dc53f
Furkan Bayraktar <me@furkanbayraktar.com>
2023-12-20 09:36:10 +0100

    Release 0.2.18

--- Formula/poly@0.2.18.rb
+++ Formula/poly@0.2.18.rb
@@ -1,4 +1,4 @@
-class Poly < Formula
+class PolyAT0218 < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.2.18/poly-0.2.18.tar.gz"
