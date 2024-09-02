b224b18a13525350a356b1dfaf412ddb054c4a04
Furkan Bayraktar <me@furkanbayraktar.com>
2024-09-02 08:19:40 +0200

    Release 0.2.20 and 0.2.21

--- Formula/poly@0.2.20.rb
+++ Formula/poly@0.2.20.rb
@@ -1,8 +1,8 @@
-class Poly < Formula
+class PolyAT0220 < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
-  url "https://github.com/polyfy/polylith/releases/download/v0.2.21/poly-0.2.21.tar.gz"
-  sha256 "8cf74e5a9e9d639911477fd6cc77393e80dca52f4d0f209425259ba408280e4e"
+  url "https://github.com/polyfy/polylith/releases/download/v0.2.20/poly-0.2.20.tar.gz"
+  sha256 "d63cffaf4229dd1d6012e1156ab1ed837f87afadfd014d7cb416ef705709cca6"
   license "EPL-1.0"
 
   uses_from_macos "ruby" => :build
--- Formula/poly@0.2.21.rb
+++ Formula/poly@0.2.21.rb
@@ -1,4 +1,4 @@
-class Poly < Formula
+class PolyAT0221 < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.2.21/poly-0.2.21.tar.gz"
