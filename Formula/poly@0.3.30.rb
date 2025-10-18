c03cded7259a8d1e76abad20e5b0306a3a7a65dc
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-10-18 13:39:10 +0200

    Release 0.3.30

--- Formula/poly@0.3.30.rb
+++ Formula/poly@0.3.30.rb
@@ -1,4 +1,4 @@
-class Poly < Formula
+class PolyAT0330 < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.3.30/poly-0.3.30.tar.gz"
