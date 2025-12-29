be4cf4b7da2b1f4f0d55a0bc234025e49f7ecc03
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-12-29 14:57:59 +0100

    Release 0.3.32 (with correct checksum)

--- Formula/poly@0.3.32.rb
+++ Formula/poly@0.3.32.rb
@@ -2,7 +2,7 @@ class PolyAT0332 < Formula
   desc "A tool used to develop Polylith based architectures in Clojure"
   homepage "https://github.com/polyfy/polylith"
   url "https://github.com/polyfy/polylith/releases/download/v0.3.32/poly-0.3.32.tar.gz"
-  sha256 "c398632e160b4d94f83c0d1ccde50e9fab75a2c4ee9abef04892522a05448bfc"
+  sha256 "b2ef2df0c7d98b05fb8740ec9709df152a13c8df1dee9a36fda4d39bc8cd4c30"
   license "EPL-1.0"
 
   uses_from_macos "ruby" => :build
